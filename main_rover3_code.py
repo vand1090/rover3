0766fbc173d53a1a050842dd222199f1fd9fa8ce
vand1090 <61294645+vand1090@users.noreply.github.com>
2020-02-24 11:12:41 -0600

    moved control and classes to functions code

--- main_rover3_code.py
+++ main_rover3_code.py
@@ -10,113 +10,15 @@ import tkinter as tk
 # Author: Alexander Vanden Bussche
 # Spring 2020
 #==========================================================
-#video steam code
-class MyVideoCapture:
-    def __init__(self, video_source=0):
-        #open video source
-        self.vid = cv2.VideoCapture('videotestsrc ! video/x-raw,framerate=14/1 ! videoscale ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
-        if not self.vid.isOpened():
-            raise ValueError("Unable to open video source ", video_source)
-        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        
-    def __del__(self):
-        if self.vid.isOpened():
-            seld.vid.release()
-        self.window.mainloop()
-        
-    def get_frame(self):
-        if self.vid.isOpened():
-            ret, frame = self.vid.read()
-            if ret:
-                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            else:
-                return(ret, None)
-        else:
-            return (ret, None)
-                
-#GUI code
-class gui(tk.Frame):
-    def dm1control(self):
-        fn.dm1()
-    def dm2control(self):
-        fn.dm2()
-    def dm3control(self):
-        fn.dm3()
-     
-    def exitBtn(self):
-        self.QUIT = tk.Button(self, text='Quit', command = tk.Frame.destroy)
-        self.QUIT.grid(column = 1, row = 1)
-    def dmOneBtn(self):
-        self.btn1 = tk.Button(self, text = 'Drive Mode 1', command = self.dm1control)
-        self.btn1.grid(column = 1, row = 2)
-    def dmTwoBtn(self):
-        self.btn1 = tk.Button(self, text = 'Drive Mode 2', command = self.dm1control)
-        self.btn1.grid(column = 1, row = 3)
-    def dmThreeBtn(self):
-        self.btn1 = tk.Button(self, text = 'Drive Mode 3', command = self.dm1control)
-        self.btn1.grid(column = 1, row = 4)
 
-    def __init__(self, master=None, video_source = 0):
-        tk.Frame.__init__(self, master)
-        self.vid = MyVideoCapture(video_source)
-        self.grid()
-        self.exitBtn()
-        self.dmOneBtn()
-        self.dmTwoBtn()
-        self.dmThreeBtn()
-
+#starts gui window using the class define above
 window = tk.Tk()
 window.geometry('350x200')
-app = gui(master=window)
+app = fn.gui(master=window)
 app.master.title('Rover Interface')
 app.mainloop()
 
 
-#THE BELOW SHOULD BE MOVED TO FUNCTIONS
-
-#Drive Modes
-#DM = 0 is waiting for input about which drive mode to be in
-#DM = 1 is 1:1 control - WASD or controller or GUI
-#DM = 2 is 7s delay with manual control
-#DM = 3 is auto mode
-#DM = 4 is for testing things in the code - skips the user input
-
-#DM = 0 selected as default
-DM = 0
-
-while(DM==0):
-    #Display options
-    print("Welcome to Rover 3 \n")
-    print("Select Drive Mode")
-
-    #wait for user input
-
-    #parse user input
-
-#========================================================
-#Drive mode 1 is manual control (WASD/Controller)
-while(DM==1):
-    print("we are in DM1")
-    #Start control GUI
-    #read inputs
-    #make a function that sends inputs to arduino
-#========================================================
-#Drive mode 2 is manual control (WASD/Controller) with delay
-while(DM==2):
-    print("we are in DM2")
-    #start control GUI
-    #read inputs
-    #make a function that sends inputs to arduino
-#========================================================
-#Drive mode 3 is automatic mode
-while(D==3):
-    print("we are in DM3")
-    #auto mode
-    #read in instructions
-    #write function to calculate heading
-    #write function to check environment
-#========================================================
 #Test code
 #testing variables
 a =  10
@@ -148,3 +50,71 @@ count = 0
     #count if used for shortening the while loop during debugging
     #count = count+1
     #output controls
+#video stream class
+#Moved to rover functions
+#many errors
+##class MyVideoCapture:
+##    #on inititalize:
+##    def __init__(self, video_source=0):
+##        #open video source
+##        self.vid = cv2.VideoCapture('videotestsrc ! video/x-raw,framerate=14/1 ! videoscale ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+##        if not self.vid.isOpened():
+##            raise ValueError("Unable to open video source ", video_source)
+##        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+##        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+##
+##     #makes sure the stream ends when you close the object   
+##    def __del__(self):
+##        if self.vid.isOpened():
+##            seld.vid.release()
+##        self.window.mainloop()
+##        
+##    def get_frame(self):
+##        if self.vid.isOpened():
+##            ret, frame = self.vid.read()
+##            if ret:
+##                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+##            else:
+##                return(ret, None)
+##        else:
+##            return (ret, None)
+#===========================================================================                
+#GUI class
+###might move to another .py file if I can do it without too many errors
+##class gui(tk.Frame):
+##    #Define control state commands
+##        #these are the commands from the buttons on the GUI
+##        #fn is the rover functions program
+##        #might change this to open a fresh GUI inside those fn.dm# functions
+##        #alternative is to change the existing GUI
+##        #Not sure which option is faster
+##    def dm1control(self):
+##        fn.dm1()
+##    def dm2control(self):
+##        fn.dm2()
+##    def dm3control(self):
+##        fn.dm3()
+##    #these are the button objects
+##    def exitBtn(self):
+##        self.QUIT = tk.Button(self, text='Quit', command = tk.Frame.destroy)
+##        self.QUIT.grid(column = 1, row = 1)
+##    def dmOneBtn(self):
+##        self.btn1 = tk.Button(self, text = 'Drive Mode 1', command = self.dm1control)
+##        self.btn1.grid(column = 1, row = 2)
+##    def dmTwoBtn(self):
+##        self.btn1 = tk.Button(self, text = 'Drive Mode 2', command = self.dm1control)
+##        self.btn1.grid(column = 1, row = 3)
+##    def dmThreeBtn(self):
+##        self.btn1 = tk.Button(self, text = 'Drive Mode 3', command = self.dm1control)
+##        self.btn1.grid(column = 1, row = 4)
+##    #initialize the frame with those button objects
+##    def __init__(self, master=None, video_source = 0):
+##        tk.Frame.__init__(self, master)
+##        self.vid = MyVideoCapture(video_source)
+##        self.grid()
+##        self.exitBtn()
+##        self.dmOneBtn()
+##        self.dmTwoBtn()
+##        self.dmThreeBtn()
+
+
--- rover_functions.py
+++ rover_functions.py
@@ -2,30 +2,134 @@ import serial
 import time
 #ser = serial.Serial('/dev/ttyACM0', 9600)
 import tkinter as tk
+#==========================================================
+# Rover 3 Control Code Functions - Raspberry Pi
+# Author: Alexander Vanden Bussche
+# Spring 2020
+#==========================================================
 
-#use as a pause between commands for debugging.
-#can also be used for drive mode 2
+#==========================================================
+#Functions
+
 def standby():
+    #use as a pause between commands for debugging.
+    #can also be used for drive mode 2
     time.sleep(5)
     #return True
 
-#This will be the software kill
+
 def kill():
+    #This will be the software kill
     DM = 0
     #stop all movement
-    #insert code
-
-    #Return to main menu
-    return DM
+    #exit the program
+    exit()
+    
 
 def dm1():
+    #Drive mode 1 is manual control (WASD/Controller)
+    #Start control GUI
+    #read inputs
+    #make a function that sends inputs to arduino
     print("Drive Mode 1")
     
 def dm2():
+    #Drive mode 2 is manual control (WASD/Controller) with delay
     print("Drive Mode 2")
+    #start control GUI
+    #read inputs
+    standby()
+    #make a function that sends inputs to arduino
     
 def dm3():
     print("Drive Mode 3")
+    #Drive mode 3 is automatic mode
+    #auto mode
+    #read in instructions
+    #write function to calculate heading
+    #write function to check environment
+#==========================================================
+#Autonomous mode functions
+def findMe():
+    #this function finds the rover in the IPS system
+
+def checkObs():
+    #Use this function to check 
+
+def avoidObs():
+    #this function tries to move the rover arund an obstacle
+#==========================================================
+#video stream class
+class MyVideoCapture:
+    #on inititalize:
+    def __init__(self, video_source=0):
+        #open video source
+        self.vid = cv2.VideoCapture('videotestsrc ! video/x-raw,framerate=14/1 ! videoscale ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source ", video_source)
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+     #makes sure the stream ends when you close the object   
+    def __del__(self):
+        if self.vid.isOpened():
+            seld.vid.release()
+        self.window.mainloop()
+        
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return(ret, None)
+        else:
+            return (ret, None)
+
+#===========================================================================                
+#GUI class
+#might move to another .py file if I can do it without too many errors
+class gui(tk.Frame):
+    #Define control state commands
+        #these are the commands from the buttons on the GUI
+        #fn is the rover functions program
+        #might change this to open a fresh GUI inside those fn.dm# functions
+        #alternative is to change the existing GUI
+        #Not sure which option is faster
+    def dm1control(self):
+        dm1()
+    def dm2control(self):
+        dm2()
+    def dm3control(self):
+        dm3()
+    def swKill(self):
+        kill()
+    #these are the button objects
+    def exitBtn(self):
+        self.QUIT = tk.Button(self, text='Quit', command = self.swKill)
+        self.QUIT.grid(column = 1, row = 1)
+    def dmOneBtn(self):
+        self.btn1 = tk.Button(self, text = 'Drive Mode 1', command = self.dm1control)
+        self.btn1.grid(column = 1, row = 2)
+    def dmTwoBtn(self):
+        self.btn2 = tk.Button(self, text = 'Drive Mode 2', command = self.dm2control)
+        self.btn2.grid(column = 1, row = 3)
+    def dmThreeBtn(self):
+        self.btn3 = tk.Button(self, text = 'Drive Mode 3', command = self.dm3control)
+        self.btn3.grid(column = 1, row = 4)
+    def killSwitch(self):
+        self.killBtn = tk.Button(self, text = 'Kill Rover Action', command = self.swKill)
+    #initialize the frame with those button objects
+    def __init__(self, master=None, video_source = 0):
+        tk.Frame.__init__(self, master)
+        self.vid = MyVideoCapture(video_source)
+        self.grid()
+        self.exitBtn()
+        self.dmOneBtn()
+        self.dmTwoBtn()
+        self.dmThreeBtn()
+
+
 #Reading GPS position. do not use for now
 #===================================================================
 def readPos():
