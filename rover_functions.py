29bd7e270d20a2f258524e7f4d973533173535ad
vand1090 <61294645+vand1090@users.noreply.github.com>
2020-02-24 16:37:48 -0600

    cleanup

--- rover_functions.py
+++ rover_functions.py
@@ -90,38 +90,9 @@ def avoidObs():
     
 def setHeading(loc, goal):
     print("set heading")
-    
-#==========================================================
-#video stream class
-# class MyVideoCapture:
-#     #on inititalize:
-#     def __init__(self, video_source=0):
-#         #open video source
-#         self.vid = cv2.VideoCapture(video_source)
-#         if not self.vid.isOpened():
-#             raise ValueError("Unable to open video source ", video_source)
-#         self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-#         self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-# 
-#      #makes sure the stream ends when you close the object   
-#     def __del__(self):
-#         if self.vid.isOpened():
-#             seld.vid.release()
-#         self.window.mainloop()
-#         
-#     def get_frame(self):
-#         if self.vid.isOpened():
-#             ret, frame = self.vid.read()
-#             if ret:
-#                 return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-#             else:
-#                 return(ret, None)
-#         else:
-#             return (ret, None)
-
+ 
 #===========================================================================                
 #GUI class
-#might move to another .py file if I can do it without too many errors
 class gui(tk.Frame):
     #Define control state commands
         #these are the commands from the buttons on the GUI
