95b16b16ab02e61a0f6e3d119ac02750f97ade87
vand1090 <61294645+vand1090@users.noreply.github.com>
2020-02-27 12:27:49 -0600

    changed test code to be used for spi development

--- testCode.py
+++ testCode.py
@@ -1,29 +1,12 @@
-import tkinter as tk
+import spidev
+import time
 
-os.system('xset r off')
-
-class Example(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent, width=400,  height=400)
-
-        self.label = tk.Label(self, text="last key pressed:  ", width=20)
-        self.label.pack(fill="both", padx=100, pady=100)
-
-        self.label.bind("<w>", self.on_wasd)
-        self.label.bind("<a>", self.on_wasd)
-        self.label.bind("<s>", self.on_wasd)
-        self.label.bind("<d>", self.on_wasd)
-
-        # give keyboard focus to the label by default, and whenever
-        # the user clicks on it
-        self.label.focus_set()
-        self.label.bind("<1>", lambda event: self.label.focus_set())
-
-    def on_wasd(self, event):
-        self.label.configure(text="last key pressed: " + event.keysym);
-
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    Example(root).pack(fill="both", expand=True)
-    root.mainloop()
+spi = spidev.SpiDev() # create spi object
+spi.open(0, 1) # open spi port 0, device (CS) 1
+try:
+    while True:
+        resp = spi.xfer2([0xAA]) # transfer one byte
+        time.sleep(0.1) # sleep for 0.1 seconds
+     #end while
+except KeyboardInterrupt: # Ctrl+C pressed, so…
+    spi.close() # … close the port before exit
