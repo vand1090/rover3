be9b1abc2384a0fe96b5f712a7f9a6ceaf8b33bf
vand1090 <61294645+vand1090@users.noreply.github.com>
2020-02-27 12:04:06 -0600

    playing witjh keypresses

--- testCode.py
+++ testCode.py
@@ -1,1 +1,29 @@
-import gpiozero
+import tkinter as tk
+
+os.system('xset r off')
+
+class Example(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent, width=400,  height=400)
+
+        self.label = tk.Label(self, text="last key pressed:  ", width=20)
+        self.label.pack(fill="both", padx=100, pady=100)
+
+        self.label.bind("<w>", self.on_wasd)
+        self.label.bind("<a>", self.on_wasd)
+        self.label.bind("<s>", self.on_wasd)
+        self.label.bind("<d>", self.on_wasd)
+
+        # give keyboard focus to the label by default, and whenever
+        # the user clicks on it
+        self.label.focus_set()
+        self.label.bind("<1>", lambda event: self.label.focus_set())
+
+    def on_wasd(self, event):
+        self.label.configure(text="last key pressed: " + event.keysym);
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    Example(root).pack(fill="both", expand=True)
+    root.mainloop()
