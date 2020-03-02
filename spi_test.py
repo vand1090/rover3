fe2a569fb493d26d58c051054b4292edac4ea2f3
vand1090 <61294645+vand1090@users.noreply.github.com>
2020-03-02 10:59:31 -0600

    testing communication protocol

--- spi_test.py
+++ spi_test.py
@@ -10,7 +10,7 @@ import spidev
 bus = 0
 
 #Device is the chip select pin. Set to 0 or 1, depending on the connections
-device = 1
+device = 0
 
 # Enable SPI
 spi = spidev.SpiDev()
@@ -28,41 +28,41 @@ spi.xfer2(msg)
 
 time.sleep(5)
 
-# Turn on one segment of each character to show that we can
-# address all of the segments
-i = 1
-while i < 0x7f:
-
-    # The decimals, colon and apostrophe dots
-    msg = [0x77]
-    msg.append(i)
-    result = spi.xfer2(msg)
-
-    # The first character
-    msg = [0x7b]
-    msg.append(i)
-    result = spi.xfer2(msg)
-
-    # The second character
-    msg = [0x7c]
-    msg.append(i)
-    result = spi.xfer2(msg)
-
-    # The third character
-    msg = [0x7d]
-    msg.append(i)
-    result = spi.xfer2(msg)
-
-    # The last character
-    msg = [0x7e]
-    msg.append(i)
-    result = spi.xfer2(msg)
-
-    # Increment to next segment in each character
-    i <<= 1
-
-    # Pause so we can see them
-    time.sleep(5)
+### Turn on one segment of each character to show that we can
+### address all of the segments
+##i = 1
+##while i < 0x7f:
+##
+##    # The decimals, colon and apostrophe dots
+##    msg = [0x77]
+##    msg.append(i)
+##    result = spi.xfer2(msg)
+##
+##    # The first character
+##    msg = [0x7b]
+##    msg.append(i)
+##    result = spi.xfer2(msg)
+##
+##    # The second character
+##    msg = [0x7c]
+##    msg.append(i)
+##    result = spi.xfer2(msg)
+##
+##    # The third character
+##    msg = [0x7d]
+##    msg.append(i)
+##    result = spi.xfer2(msg)
+##
+##    # The last character
+##    msg = [0x7e]
+##    msg.append(i)
+##    result = spi.xfer2(msg)
+##
+##    # Increment to next segment in each character
+##    i <<= 1
+##
+##    # Pause so we can see them
+##    time.sleep(5)
 
 
 # Clear display again
