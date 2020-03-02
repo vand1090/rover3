47efd80205ea81b9be01a3bf99766195f61cafd5
vand1090 <61294645+vand1090@users.noreply.github.com>
2020-03-02 12:54:52 -0600

    minor edits

--- i2c_test.py
+++ i2c_test.py
@@ -1,2 +1,13 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import smbus
 import time
-import 
+
+address = 0x38
+
+bus = smbus.SMBus(1)
+while True:
+    bus.write_byte(address,0xEF)
+    time.sleep(0.5)
+    bus.write_byte(address,0xFF)
+time.sleep(0.5)
