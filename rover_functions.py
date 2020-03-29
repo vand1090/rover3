d34c9d9ac8477785beaf60b42dc0b3377d09f1d4
vand1090 <61294645+vand1090@users.noreply.github.com>
2020-03-29 16:18:42 -0500

    tof_sensor

--- rover_functions.py
+++ rover_functions.py
@@ -4,6 +4,9 @@ import time
 import tkinter as tk
 import cv2
 from picamera import PiCamera
+import board
+import busio
+import adafruit_vl53l0x
 #from keybinder import KeyBinder
 #==========================================================
 # Rover 3 Control Code Functions - Raspberry Pi
@@ -14,6 +17,8 @@ from picamera import PiCamera
 camera = PiCamera()
 
 restart = True
+i2c = busio.I2C(board.SCL, board.SDA)
+#tof_sensor = adafruit_vl53l0x.VL53L0X(i2c)
 #==========================================================
 #Functions
 
@@ -22,7 +27,8 @@ def standby():
     #can also be used for drive mode 2
     time.sleep(5)
     #return True
-
+def getRange():
+   # print('Range: {}mm'.format(tof_sensor.range))
 
 def kill():
     #This will be the software kill
