782512ba691e2221e35b1d109696488369b9a451
vand1090 <61294645+vand1090@users.noreply.github.com>
2020-04-12 18:30:00 -0500

    fixed gui code

--- rover_functions.py
+++ rover_functions.py
@@ -1,14 +1,14 @@
-import serial
+#import serial
 import time
 #ser = serial.Serial('/dev/ttyACM0', 9600)
 import tkinter as tk
-import cv2
-from picamera import PiCamera
-import smbus
+# import cv2
+# from picamera import PiCamera
+# import smbus
 
-import board
-import busio
-import adafruit_vl53l0x
+# import board
+# import busio
+# import adafruit_vl53l0x
 from debouncer import Debouncer
 #from keybinder import KeyBinder
 #==========================================================
@@ -17,12 +17,12 @@ from debouncer import Debouncer
 # Spring 2020
 #==========================================================
 #constants
-camera = PiCamera()
+# camera = PiCamera()
 drive = 6
 restart = True
-i2c = busio.I2C(board.SCL, board.SDA)
-bus = smbus.SMBus(1)
-tof_sensor = adafruit_vl53l0x.VL53L0X(i2c)
+# i2c = busio.I2C(board.SCL, board.SDA)
+# bus = smbus.SMBus(1)
+# tof_sensor = adafruit_vl53l0x.VL53L0X(i2c)
 duino_address = 0x04
 
 LARGE_FONT= ("Verdana", 12)
@@ -35,19 +35,19 @@ def standby():
     #can also be used for drive mode 2
     time.sleep(5)
     #return True
-def getRange():
-   print('Range: {}mm'.format(tof_sensor.range))
-   #time.sleep(1)
-   return tof_sensor.range
+# def getRange():
+#    print('Range: {}mm'.format(tof_sensor.range))
+#    #time.sleep(1)
+#    return tof_sensor.range
 def driving(mode):
-    bus.write_byte(duino_address,mode)
-    readNum()
-    #time.sleep(0.5)
+    #bus.write_byte(duino_address,mode)
+    #readNum()
+    time.sleep(0.5)
 
-def readNum():
-    num = bus.read_byte(duino_address)
-    print("RPI Received", num)
-    return num
+# def readNum():
+#     num = bus.read_byte(duino_address)
+#     print("RPI Received", num)
+#     return num
 
 def kill():
     #This will be the software kill
@@ -57,11 +57,11 @@ def kill():
     #exit the program
     exit()
 
-def startCam():
-    camera.start_preview(fullscreen=False,window=(0,0,200,200)) #xywh
+# def startCam():
+#     camera.start_preview(fullscreen=False,window=(0,0,200,200)) #xywh
 
-def endCam():
-    camera.stop_preview()
+# def endCam():
+#     camera.stop_preview()
 
 def dm1():
     #Drive mode 1 is manual control (WASD/Controller)
@@ -82,17 +82,17 @@ def dm2():
 
 def dm3():
     print("Drive Mode 3")
-    goal_loc = 1
-    count = 1
-    drive = True
-    while(drive):
-        loc = findMe()
-        setHeading(loc, goal_loc)
-        if(checkObs() == 2):
-            avoidObs(2)
-        count = count+1
-        if(count == 10):
-            drive=False
+    # goal_loc = 1
+    # count = 1
+    # drive = True
+    # while(drive):
+    #     loc = findMe()
+    #     setHeading(loc, goal_loc)
+    #     if(checkObs() == 2):
+    #         avoidObs(2)
+    #     count = count+1
+    #     if(count == 10):
+    #         drive=False
 
     #Drive mode 3 is automatic mode
     #auto mode
@@ -109,7 +109,7 @@ def findMe():
 def checkObs():
     rangeMin = 30 #Rover allowed no closer than this (mm)
     rangeAvoid = 100
-    rangeRead = getRange() #Reads the LIDAR
+    rangeRead = 0#getRange() #Reads the LIDAR
     #If there's an obstruction within range, return true
     if (rangeRead <= rangeMin):
         print("WARNING: Too Close")
@@ -142,15 +142,6 @@ def setHeading(loc, goal):
 #===========================================================================
 #GUI class
 class gui(tk.Tk):
-    #Define control state commands
-        #these are the commands from the buttons on the GUI
-        #fn is the rover functions program
-        #might change this to open a fresh GUI inside those fn.dm# functions
-        #alternative is to change the existing GUI
-        #Not sure which option is faster
-    #these are the button objects
-
-
     def keyBind(self):
         self.label = tk.Label(self, text="Key Press:  ", width=20)
         self.debouncer = Debouncer(self.on_w,self.off_w)
@@ -170,7 +161,7 @@ class gui(tk.Tk):
         self.label.grid(column = 1, row = 5)
 
     def on_w(self, event):
-        self.label.configure(text="Forward");
+        self.label.configure(text="Forward")
         if dm1:
             driving(2)
             self.after(20)
@@ -180,7 +171,7 @@ class gui(tk.Tk):
     def off_w(self, event):
         driving(6)
     def on_a(self, event):
-        self.label.configure(text="Left");
+        self.label.configure(text="Left")
         if dm1:
             driving(4)
         elif dm2:
@@ -189,7 +180,7 @@ class gui(tk.Tk):
     def off_a(self, event):
         driving(6)
     def on_s(self, event):
-        self.label.configure(text="Backward");
+        self.label.configure(text="Backward")
         if dm1:
             driving(3)
         elif dm2:
@@ -198,7 +189,7 @@ class gui(tk.Tk):
     def off_s(self, event):
         driving(6)
     def on_d(self, event):
-        self.label.configure(text="Right");
+        self.label.configure(text="Right")
         if dm1:
             driving(5)
         elif dm2:
@@ -209,11 +200,12 @@ class gui(tk.Tk):
     def on_x(self, event):
         driving(6)
 
-    def on_wasd(newWindow, event):
-        newWindow.label.configure(text="last key pressed: " + event.keysym);
+    def on_wasd(self, event):
+        self.label.configure(text="last key pressed: " + event.keysym)
 
 
-    #initialize the frame with those button objects
+    #initialize base frame as an array of 4 possible windows
+    #main menu, DM1, Dm2, Dm3
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
@@ -223,126 +215,185 @@ class gui(tk.Tk):
         #self.grid()
         self.windows = {}
 
-        for F in (mainMenu, dm1Page):
+        #initializes each window so it can be shown later
+        for F in (mainMenu, dm1Page, dm2Page, dm3Page):
             frame = F(container, self)
             self.windows[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
         self.show_new_window(mainMenu)
 
+    #start by showing main menu
     def show_new_window(self, cont):
         frame = self.windows[cont]
         frame.tkraise()
 
 
+#this is the first thing that will open
 class mainMenu(tk.Frame):
-
-    def exitBtn(self):
-        self.QUIT = tk.Button(self, text='Quit', command = self.swKill)
-        self.QUIT.grid(column = 1, row = 1)
-    def rstBtn(self):
-        self.QUIT = tk.Button(self, text='Restart', command = self.resetSwitch)
-        self.QUIT.grid(column = 2, row = 1)
-    def dmOneBtn(self):
-        self.btn1 = tk.Button(self, text = 'Drive Mode 1', command = self.dm1control)
-        self.btn1.grid(column = 1, row = 2)
-
-    def dmTwoBtn(self):
-        self.btn2 = tk.Button(self, text = 'Drive Mode 2', command = self.dm2control)
-        self.btn2.grid(column = 1, row = 3)
-    def dmThreeBtn(self):
-        self.btn3 = tk.Button(self, text = 'Drive Mode 3', command = self.dm3control)
-        self.btn3.grid(column = 1, row = 4)
-
-    def dm1control(self):
-        endCam()
-        startCam()
-        #newWindow = tk.Toplevel(self)
-        #dm1_quitBtn = tk.Button(newWindow, text='Quit', command = newWindow.destroy)
-        #dm1_quitBtn.pack()
-        dm1()
-
-    def dm2control(self):
-        endCam()
-        startCam()
-        dm2()
-    def dm3control(self):
-        endCam()
-        #Disabled camera during testing
-
-        #startCam()
-        dm3()
-    def swKill(self):
-        endCam()
-        kill()
-    def killSwitch(self):
-        self.killBtn = tk.Button(self, text = 'Kill Rover Action', command = self.swKill)
-    def resetSwitch(self):
-        endCam()
-        self.destroy
-        self.__init__
-
     def __init__(self, parent, controller):
-        #tk.Frame.__init__(self,parent)
-        #label = tk.Label(self, text="Start Page", font=LARGE_FONT)
-        #label.pack(pady=10,padx=10)
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Select Drive Mode", font=LARGE_FONT)
+        label.grid(column=1, row=1)
 
-        #button = tk.Button(self, text="Visit Page 1", command=lambda: controller.show_frame(driveModeOnePage))
-        #button.pack()
-
-        tk.Frame.__init__(self, parent)
-        #self.vid = MyVideoCapture(video_source)
+        #show all the buttons, generated below
         self.grid()
         self.exitBtn()
         self.rstBtn()
-        self.dmOneBtn()
-        self.dmTwoBtn()
-        self.dmThreeBtn()
+        self.dmOneBtn(controller)
+        self.dmTwoBtn(controller)
+        self.dmThreeBtn(controller)
+
+        #move this to control modes:
         #self.keyBind()
 
+    
+    def exitBtn(self): #kill the program
+        self.QUIT = tk.Button(self, text='Quit', command = self.swKill)
+        self.QUIT.grid(column = 1, row = 2)
+    def rstBtn(self): # restarts main menu
+        self.QUIT = tk.Button(self, text='Restart', command = self.resetSwitch)
+        self.QUIT.grid(column = 2, row = 2)
 
-class d(tk.Frame):
+    def dmOneBtn(self, controller): #bring up drive mode 1
+        self.btn1 = tk.Button(self, text = 'Drive Mode 1', command =lambda: controller.show_new_window(dm1Page))
+        self.btn1.grid(column = 1, row = 3)
+
+    def dmTwoBtn(self, controller): #bring up drive mode 2
+        self.btn2 = tk.Button(self, text = 'Drive Mode 2', command =lambda: controller.show_new_window(dm2Page))
+        self.btn2.grid(column = 1, row = 4)
+
+    def dmThreeBtn(self, controller): #bring up drive mode 3
+        self.btn3 = tk.Button(self, text = 'Drive Mode 3', command =lambda: controller.show_new_window(dm3Page))
+        self.btn3.grid(column = 1, row = 5)
+
+    def swKill(self): # stops all rover action
+        #endCam()
+        kill()
+
+    def killSwitch(self):
+        self.killBtn = tk.Button(self, text = 'Kill Rover Action', command = self.swKill)
+
+
+    def resetSwitch(self): #function for reset button
+        #endCam()
+        self.destroy
+        self.__init__
+
+    
+#here begins the gui for drive mode 1. Implements a keylistener
+class dm1Page(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Drive Mode 1", font=LARGE_FONT)
+        label.grid(column=1, row=1)
+        
+        self.grid()
+        self.exitBtn()
+        self.mainMenuBtn(controller)
+
+        #startCam()
+        #dm1()
+
+    def exitBtn(self):
+        self.QUIT = tk.Button(self, text='Quit', command = self.swKill)
+        self.QUIT.grid(column = 1, row = 2)
+    
+    def mainMenuBtn(self, controller):
+        self.btnMM = tk.Button(self, text = 'Main Menu', command =lambda: controller.show_new_window(mainMenu))
+        self.btnMM.grid(column = 2, row = 2)
+
+    def swKill(self):
+        #endCam()
+        kill()
+
+class dm2Page(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Drive Mode 2", font=LARGE_FONT)
+        label.grid(column=1, row=1)
+        
+        self.grid()
+        self.exitBtn()
+        self.mainMenuBtn(controller)
+
+        #startCam()
+        #dm2()
+
+    def exitBtn(self):
+        self.QUIT = tk.Button(self, text='Quit', command = self.swKill)
+        self.QUIT.grid(column = 1, row = 2)
+    
+    def mainMenuBtn(self, controller):
+        self.btnMM = tk.Button(self, text = 'Main Menu', command =lambda: controller.show_new_window(mainMenu))
+        self.btnMM.grid(column = 2, row = 2)
+
+    def swKill(self):
+        #endCam()
+        kill()
 
 
+class dm3Page(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Drive Mode 3", font=LARGE_FONT)
+        label.grid(column=1, row=1)
+        
+        self.grid()
+        self.exitBtn()
+        self.mainMenuBtn(controller)
+
+        #startCam()
+        #dm2()
+
+    def exitBtn(self):
+        self.QUIT = tk.Button(self, text='Quit', command = self.swKill)
+        self.QUIT.grid(column = 1, row = 2)
+    
+    def mainMenuBtn(self, controller):
+        self.btnMM = tk.Button(self, text = 'Main Menu', command =lambda: controller.show_new_window(mainMenu))
+        self.btnMM.grid(column = 2, row = 2)
+
+    def swKill(self):
+        #endCam()
+        kill()
 
 #Reading GPS position. do not use for now
 #===================================================================
-def readPos():
-    try:
-        state = ser.readline()
-        state_conv = state.decode("utf-8")
-        pos = parsePos(state_conv)
-        #print(state_conv)
-        return pos
-    except:
-        pass
+# def readPos():
+#     try:
+#         state = ser.readline()
+#         state_conv = state.decode("utf-8")
+#         pos = parsePos(state_conv)
+#         #print(state_conv)
+#         return pos
+#     except:
+#         pass
 
-def parsePos(state_conv):
-    posType = state_conv.split(',')
-    if (posType[0] == "$GPRMC"):
-        # pos = posType[1].split(',')
-        # print(pos)
-        timeRead = posType[1]
-        validBool = posType[2]
-        # print(validBool)
-        if (validBool) == "V":
-            print("No Connection to Satellites at", timeRead)
-            # print(timeRead)
-            return 0
-        elif(validBool == "A"):
-            latitude = posType[3]
-            lat_dir = posType[4]
-            longitude = postype[5]
-            long_dir = posType[6]
-            roverSpeed = posType[7]
-        return [latitude, lat_dir, longitude, long_dir, roverSpeed]
+# def parsePos(state_conv):
+#     posType = state_conv.split(',')
+#     if (posType[0] == "$GPRMC"):
+#         # pos = posType[1].split(',')
+#         # print(pos)
+#         timeRead = posType[1]
+#         validBool = posType[2]
+#         # print(validBool)
+#         if (validBool) == "V":
+#             print("No Connection to Satellites at", timeRead)
+#             # print(timeRead)
+#             return 0
+#         elif(validBool == "A"):
+#             latitude = posType[3]
+#             lat_dir = posType[4]
+#             longitude = postype[5]
+#             long_dir = posType[6]
+#             roverSpeed = posType[7]
+#         return [latitude, lat_dir, longitude, long_dir, roverSpeed]
 
-    elif(posType[0] == "$GPGGA"):
-        time_ping= posType[1]
-        latitude = posType[2]
-        lat_dir = posType[3]
-        longitude = posType[4]
-        long_dir = posType[5]
-        return [time_ping, latitude, lat_dir, longitude, long_dir]
-#===================================================================
+#     elif(posType[0] == "$GPGGA"):
+#         time_ping= posType[1]
+#         latitude = posType[2]
+#         lat_dir = posType[3]
+#         longitude = posType[4]
+#         long_dir = posType[5]
+#         return [time_ping, latitude, lat_dir, longitude, long_dir]
+# #===================================================================
